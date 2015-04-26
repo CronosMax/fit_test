e8e077dc5464d46337b7af94e44d04542e07bc5f
Cronos <Cronos@Cronos-PC>
2015-04-26 16:00:16 +0300

    get started

---
+++ config/db.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=fit_test',
+    'username' => 'root',
+    'password' => '',
+	'tablePrefix' => 'tbl_',
+    'charset' => 'utf8',
+];
